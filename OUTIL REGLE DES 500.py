3a34628d4cccf9a2c40e1ac468544e4061eab995
Clémentin <79161093+clementin-bonneau-riera@users.noreply.github.com>
2021-07-13 23:55:36 +0200

    Add files via upload

---
+++ OUTIL REGLE DES 500.py
@@ -0,0 +1,17 @@
+print("Outil pour calculer la règle des 500 en photographie pour les appareils à capteurs APS-C (x1.6 chez Canon, x1.5 chez Nikon) ou micro 4/3 (x2).")
+print("Pour un capteur full-frame 24x36 ; facteur de conversion = x1.")
+
+#juste pour faire un peu + pro ;)
+version = "0.0.2"
+
+focal = eval(input("longueur focale : "))
+convert = eval(input("facteur de conversion : "))
+
+time = (500/(focal*convert)) #pr calculer grâçe à la règle des 500
+
+print(str(time) + " secondes")
+
+#affichage en fin de programme 
+print(".")
+print("v" + str(version) + " du programmme")
+#créé par Clémz
