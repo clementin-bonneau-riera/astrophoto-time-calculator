ff1109cc0ac68f80add3cde45e28e978913f5c61
Clémentin <79161093+clementin-bonneau-riera@users.noreply.github.com>
2021-09-18 11:15:45 +0200

    time int type, nombres entiers et plus décimaux

--- script.py
+++ script.py
@@ -1,12 +1,12 @@
 print("Outil pour calculer la règle des 500 en photographie pour les appareils à capteurs APS-C (x1.6 chez Canon, x1.5 chez Nikon) ou micro 4/3 (x2).")
 print("Pour un capteur full-frame 24x36 ; facteur de conversion = x1.")
 
-version = "0.0.2"
+version = "0.0.3"
 
 focal = float(input("longueur focale : "))
 convert = float(input("facteur de conversion : "))
 
-time = (500/(focal*convert)) #pr calculer grâçe à la règle des 500
+time = int((500/(focal*convert))) #pr calculer grâçe à la règle des 500
 
 print(str(time) + " secondes")
 
